1553b8b228cb9645afda1f8d06def753ae503f82
sryerabati <sryerabati@stu.naperville203.org>
2024-01-26 13:17:36 -0600

    THIS IS A START!

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
-import logo from './logo.png';
+import logo from './logo1.png';
 
 function App() {
   return (
     <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        
+         </header>
+      <p>
           Welcome to the SlideCentral!
-        </p>
-      </header>
+      </p>
     </div>
   );
 }
