d6a6f83bf7f330e3047a395d7532284f940d4ee6
sryerabati <sryerabati@stu.naperville203.org>
2024-04-18 13:30:17 -0500

    template

--- my-app/src/assets/HomeScreen/CarouselItem2.jsx
+++ my-app/src/assets/HomeScreen/CarouselItem2.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import './Carousel.css';
 
-export const CarouselItem = ({ item, isFullScreen }) => {
+export const CarouselItem = ({ width, isFullScreen }) => {
   return (
-    <div className={`carousel-item ${isFullScreen ? 'fullscreen' : ''}`}>
-        
-        <div></div>
-        <img className = "carousel-img" src = {item.icon} />
-        
+    <div className={`carousel-item ${isFullScreen ? 'fullscreen' : ''} ` } style={{ width: width }}>
+        <div className="carousel-item-content">
+            <h2>SlideCetnral</h2>
+            <p>We meet every monday</p>
+
+        {/* <img className = "carousel-img" src = {item.icon} /> */}
+        </div>
     </div>
   );
 }
