732f0d8eed8d6232b4ee72801827de5cacaa1e91
connorfurby <111527458+connorfurby@users.noreply.github.com>
2024-05-02 12:47:51 -0500

    x

--- my-app/src/assets/Dashboard/SlideCreation/SlideCreationHomePage.jsx
+++ my-app/src/assets/Dashboard/SlideCreation/SlideCreationHomePage.jsx
@@ -1,11 +1,11 @@
-
 import styles from './slidecreation.module.css';
 import moment from 'moment';
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-
-
 const currentDateTime = moment().format('YYYY-MM-DD HH:mm:ss');
+
+
+
 
 const SlideCreationHomePage = () => {
 
@@ -14,96 +14,205 @@ const SlideCreationHomePage = () => {
   const [dateInput, setDateInput] = useState('');
   const [imagePath, setImagePath] = useState(null);
   const [imagePath2, setImagePath2] = useState(null);
-  const [currentStep, setCurrentStep] = useState(1);
-  const totalSteps = 8; // total number of steps in the form
+  const [currentStep, setCurrentStep] = useState(0);
   const [backgroundColor, setBackgroundColor] = useState('#ffffff'); 
-  const [textColor, setTextColor] = useState('#000000'); 
-
-  const handleTitleChange = (event) => {
-    setTitleInput(event.target.value);
-};
-const handleDescriptionChange = (event) => {
-  setDescriptionInput(event.target.value);
-};
-const handleDateChange = (event) => {
-  setDateInput(event.target.value);
-};
-
-  const handleCreateSlide = () => {
-    // Logic for creating a slide
-    console.log('Create Slide button clicked');
-
-    const slideID = titleInput.replace(/ /g, '').toLowerCase() + currentDateTime.replace(/-/g, '').replace(/:/g, '').replace(/ /g, '');
-    
-
-    axios.post('http://localhost:5000/api/slide', { sub1: slideID , sub2: titleInput, sub3: descriptionInput, sub4: dateInput, sub5: sessionStorage.getItem("currentActivityID"), sub6: imagePath, sub7:backgroundColor, sub8: textColor })
-
-
-  };
-
-
-
-
+  const [textColor, setTextColor] = useState('#000000');
+  const [creationMethod, setCreationMethod] = useState(null);
   const [selectedFile, setSelectedFile] = useState(null);
   const [selectedFile2, setSelectedFile2] = useState(null);
   const [preview, setPreview] = useState();
   const [preview2, setPreview2] = useState();
 
-    useEffect(() => {
-        if (!selectedFile) {
-            setPreview(undefined);
-            return;
-        }
 
-        const objectUrl = URL.createObjectURL(selectedFile);
-        setPreview(objectUrl);
+  // SlideTitleStep Component
+  const SlideTitleStep = ({ titleInput, handleTitleChange }) => (
+    <div className={styles.inputContainer}>
+      <div className={styles.inputTitle}>Slide Title</div>
+      <input 
+        value={titleInput} 
+        onChange={handleTitleChange} 
+        type="text" 
+        className={styles.slideTitleInput}
+        placeholder="Enter Slide Title Here" 
+      />
+    </div>
+  );
 
-        return () => URL.revokeObjectURL(objectUrl);
-    }, [selectedFile]);
+  // DescriptionStep Component
+  const DescriptionStep = ({ descriptionInput, handleDescriptionChange }) => (
+    <div className={styles.inputContainer}>
+      <div className={styles.inputTitle}>Activity Description / Message</div>
+      <textarea 
+        value={descriptionInput} 
+        onChange={handleDescriptionChange} 
+        className={styles.slideTitleInput}
+        placeholder="Enter Activity Description Here" 
+      />
+    </div>
+  );
 
-    const onSelectFile = e => {
-      if (!e.target.files || e.target.files.length === 0) {
-          setSelectedFile(undefined);
+  // EventDateStep Component
+  const EventDateStep = ({ dateInput, handleDateChange }) => (
+    <div className={styles.inputContainer}>
+      <div className={styles.inputTitle}>Event Date</div>
+      <input 
+        value={dateInput} 
+        onChange={handleDateChange} 
+        type="date" 
+        className={styles.slideTitleInput}
+      />
+    </div>
+  );
+
+  // UploadImageStep Component
+  const UploadImageStep = ({ selectedFile, setSelectedFile, preview, handleUploadSlide, imageKey }) => (
+    <div className={styles.inputContainer}>
+      <div className={styles.inputTitle}>Import Image</div>
+      <input 
+        key={selectedFile ? 'uploaded' : 'not-uploaded'}
+        type="file" 
+        name="image" 
+        id={`imageInput-${imageKey}`} 
+        accept="image/*" 
+        onChange={e => setSelectedFile(e.target.files[0])}
+      />
+      {selectedFile && <img src={preview} className={styles.imagePreview} />}
+      <button onClick={handleUploadSlide} className={styles.uploadButton}>Upload</button>
+    </div>
+  );
+
+  const UploadImageStep2 = ({ selectedFile, setSelectedFile, preview, handleUploadSlide, imageKey }) => (
+    <div className={styles.inputContainer}>
+      <div className={styles.inputTitle}>Import Image</div>
+      <input 
+        key={selectedFile ? 'uploaded' : 'not-uploaded'}
+        type="file" 
+        name="image" 
+        id={`imageInput-${imageKey}`} 
+        accept="image/*" 
+        onChange={e => setSelectedFile(e.target.files[0])}
+      />
+      {selectedFile && <img src={preview} className={styles.imagePreview} />}
+      <button onClick={handleUploadSlide} className={styles.uploadButton}>Upload</button>
+    </div>
+  );
+
+  // BackgroundColorStep Component
+  const BackgroundColorStep = ({ backgroundColor, setBackgroundColor }) => (
+    <div className={styles.inputContainer}>
+      <div className={styles.inputTitle}>Background Color</div>
+      <input
+        type="color"
+        className={styles.colorInput}
+        value={backgroundColor}
+        onChange={e => setBackgroundColor(e.target.value)}
+      />
+    </div>
+  );
+
+  // TextColorStep Component
+  const TextColorStep = ({ textColor, setTextColor }) => (
+    <div className={styles.inputContainer}>
+      <div className={styles.inputTitle}>Text Color</div>
+      <input
+        type="color"
+        className={styles.colorInput}
+        value={textColor}
+        onChange={e => setTextColor(e.target.value)}
+      />
+    </div>
+  );
+
+  // CreateSlideStep Component
+  const CreateSlideStep = ({ handleCreateSlide }) => (
+    <div className={styles.inputContainer}>
+      <div className={styles.inputTitle}>Create Slide Templates!</div>
+      <button className={styles.createButton} onClick={handleCreateSlide}>
+        Create Slide
+      </button>
+    </div>
+  );
+
+
+
+  const handleMethodSelection = (method) => {
+    setCreationMethod(method);
+    setCurrentStep(1); 
+  };
+
+  const totalSteps = creationMethod === 'form' ? 8 : 3;
+
+  const renderInitialQuestion = () => (
+    <div className={styles.inputContainer}>
+      <div className={styles.inputTitle}>How would you like to create a slide?</div>
+      <button onClick={() => handleMethodSelection('form')} className={styles.optionButton}>Generate with Form</button>
+      <button onClick={() => handleMethodSelection('image')} className={styles.optionButton}>Upload Slide Image</button>
+    </div>
+  );
+
+  const handleTitleChange = (event) => {
+    setTitleInput(event.target.value);
+  };
+  const handleDescriptionChange = (event) => {
+    setDescriptionInput(event.target.value);
+  };
+  const handleDateChange = (event) => {
+    setDateInput(event.target.value);
+  };
+
+  const handleCreateSlide = () => {
+    console.log('Create Slide button clicked');
+    const slideID = titleInput.replace(/ /g, '').toLowerCase() + currentDateTime.replace(/-/g, '').replace(/:/g, '').replace(/ /g, '');
+    axios.post('http://localhost:5000/api/slide', { sub1: slideID , sub2: titleInput, sub3: descriptionInput, sub4: dateInput, sub5: sessionStorage.getItem("currentActivityID"), sub6: imagePath, sub7:backgroundColor, sub8: textColor })
+  };
+
+  useEffect(() => {
+    if (!selectedFile) {
+      setPreview(undefined);
           return;
-      }
-  
-      setSelectedFile(e.target.files[0]);
+    }
+
+    const objectUrl = URL.createObjectURL(selectedFile);
+    setPreview(objectUrl);
+    return () => URL.revokeObjectURL(objectUrl);
+  }, [selectedFile]);
+
+  const onSelectFile = e => {
+    if (!e.target.files || e.target.files.length === 0) {
+      setSelectedFile(undefined);
+      return;
+    }
+    setSelectedFile(e.target.files[0]);
   };
   
   const onSelectFile2 = e => {
-      if (!e.target.files || e.target.files.length === 0) {
-          setSelectedFile2(undefined);
-          return;
-      }
-  
-      setSelectedFile2(e.target.files[0]);
+    if (!e.target.files || e.target.files.length === 0) {
+      setSelectedFile2(undefined);
+      return;
+    }
+    setSelectedFile2(e.target.files[0]);
   };
 
-    useEffect(() => {
-      if (!selectedFile2) {
-          setPreview2(undefined);
-          return;
+  useEffect(() => {
+    if (!selectedFile2) {
+      setPreview2(undefined);
+        return;
       }
-  
       const objectUrl2 = URL.createObjectURL(selectedFile2);
       setPreview2(objectUrl2);
-  
       return () => URL.revokeObjectURL(objectUrl2);
   }, [selectedFile2]);
-
-
 
   const [key1, setKey1] = useState(Date.now());
   const [key2, setKey2] = useState(Date.now());
 
   const handleUploadSlide = async e => {
     e.preventDefault();
-  
     if (!selectedFile || !selectedFile2) {
       alert('No file selected for upload.');
       return;
     }
-  
+
     if (
       (selectedFile.type !== 'image/jpeg' && selectedFile.type !== 'image/png' && selectedFile.type !== 'image/jpg') ||
       (selectedFile2.type !== 'image/jpeg' && selectedFile2.type !== 'image/png' && selectedFile2.type !== 'image/jpg')
@@ -172,152 +281,89 @@ const handleDateChange = (event) => {
   // Handle arrow key press to navigate the form
   useEffect(() => {
     const handleKeyDown = (event) => {
-      if (event.key === 'ArrowRight') {
+      if (event.key === 'ArrowRight' || event.key === 'Enter' && currentStep > 0) {
         goNext();
-      } else if (event.key === 'ArrowLeft') {
+      } else if (event.key === 'ArrowLeft' && currentStep > 1) {
         goBack();
       }
     };
-
     window.addEventListener('keydown', handleKeyDown);
-    
-    // Cleanup listener
     return () => window.removeEventListener('keydown', handleKeyDown);
-  }, [currentStep]);
+  }, [currentStep, creationMethod]);
 
 
   const renderStepContent = () => {
+    if (currentStep === 0) return renderInitialQuestion();
+
+    if (creationMethod === 'form') {
+      return renderFormSteps();
+    } else if (creationMethod === 'image') {
+      return renderImageUploadSteps();
+    }
+  };
+
+  const renderFormSteps = () => {
     switch (currentStep) {
       case 1:
-        return (
-          <div className={styles.inputContainer}>
-            <div className={styles.inputTitle}>1. Slide Title</div>
-            <input 
-              value={titleInput} 
-              onChange={handleTitleChange} 
-              type="text" 
-              className={styles.slideTitleInput}
-              placeholder="Enter Slide Title Here" 
-            />
-          </div>
-        );
+        return <SlideTitleStep />;
       case 2:
-        return (
-          <div className={styles.inputContainer}>
-            <div className={styles.inputTitle}>2. Activity Description / Message</div>
-            <textarea 
-              value={descriptionInput} 
-              onChange={handleDescriptionChange} 
-              className={styles.slideTitleInput}
-              placeholder="Enter Activity Description Here" 
-            />
-          </div>
-        );
+        return <DescriptionStep />;
       case 3:
-        return (
-          <div className={styles.inputContainer}>
-            <div className={styles.inputTitle}>3. Event Date</div>
-            <input 
-              value={dateInput} 
-              onChange={handleDateChange} 
-              type="date" 
-              className={styles.slideTitleInput}
-            />
-          </div>
-        );
+        return <EventDateStep />;
       case 4:
-        return (
-          <div className={styles.inputContainer}>
-            <div className={styles.inputTitle}>4. Import Image 1</div>
-            <input 
-              key={key1}
-              type="file" 
-              name="image" 
-              id="imageInput" 
-              accept="image/*" 
-              onChange={onSelectFile}
-            />
-            {selectedFile && <img id="output" src={preview} className={styles.imagePreview} />}
-            <button onClick={handleUploadSlide} className={styles.uploadButton}>Upload</button>
-          </div>
-        );
+        return <UploadImageStep imageKey='image1' />;
       case 5:
-        return (
-          <div className={styles.inputContainer}>
-            <div className={styles.inputTitle}>5. Import Image 2</div>
-            <input
-              key={key2}
-              type="file"
-              name="image2"
-              accept="image/*"
-              onChange={onSelectFile2}
-            />
-            {/* Display the second image preview if available */}
-            {selectedFile2 && <img id="output" src={preview2} className={styles.imagePreview} />}
-            <button onClick={handleUploadSlide} className={styles.uploadButton}>Upload</button>
-          </div>
-        );
+        return <UploadImageStep2 imageKey='image2' />;
       case 6:
-        return (
-          <div className={styles.inputContainer}>
-            <div className={styles.inputTitle}>6. Background Color</div>
-            <input
-              type="color"
-              className={styles.colorInput}
-              value = {backgroundColor}
-              onChange={(event) => setBackgroundColor(event.target.value)} // Update backgroundColor when the user selects a color
-            />
-          </div>
-        );
+        return <BackgroundColorStep />;
       case 7:
-        return (
-          <div className={styles.inputContainer}>
-            <div className={styles.inputTitle}>7. Text Color</div>
-            <input
-              type="color"
-              className={styles.colorInput}
-              value = {textColor}
-              onChange={(event) => setTextColor(event.target.value)} // Update textColor when the user selects a color
-            />
-          </div>
-        );
+        return <TextColorStep />;
       case 8:
-        return (
-          <div className={styles.inputContainer}>
-            <div className={styles.inputTitle}>8. Create Slide Templates!</div>
-            <button className={styles.createButton} onClick = {handleCreateSlide}>
-              Create Slide
-            </button>
-          </div>
-        );
+        return <CreateSlideStep />;
+      default:
+        return null;
+    }
+  };
+
+  const renderImageUploadSteps = () => {
+    switch (currentStep) {
+      case 1:
+        return <EventDateStep />;
+      case 2:
+        return <UploadImageStep imageKey='image' />;
+      case 3:
+        return <CreateSlideStep />;
       default:
         return null;
     }
   };
   
+
   return (
     <div className={styles.slideCreationHomePage}>
       <div className={styles.slideCreationContainer}>
         {renderStepContent()}
         <div className={styles.navigationButtons}>
           <button 
-            onClick={currentStep > 1 ? goBack : undefined}
+            onClick={goBack}
+            disabled={currentStep <= 1}
             className={`${styles.navButton} ${styles.navButtonLeft}`}
-            disabled={currentStep === 1} // Disables button on first step
           >
             <span className="material-symbols-outlined">arrow_back_ios</span>
           </button>
           <button 
-            onClick={currentStep < totalSteps ? goNext : undefined}
+            onClick={goNext}
+            disabled={currentStep >= totalSteps || currentStep === 0}
             className={`${styles.navButton} ${styles.navButtonRight}`}
-            disabled={currentStep === totalSteps} // Disables button on last step
           >
             <span className="material-symbols-outlined">arrow_forward_ios</span>
           </button>
         </div>
-        <div className={styles.stepCounter}>
-          <p>Step {currentStep} of {totalSteps}</p>
-        </div>
+        {currentStep > 0 && (
+          <div className={styles.stepCounter}>
+            <p>Step {currentStep} of {totalSteps}</p>
+          </div>
+        )}
       </div>
     </div>
   );
